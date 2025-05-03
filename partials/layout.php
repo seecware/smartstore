4eac1a44f75cf4af5df28c66bbc1cd843caf84ae
Francisco Mendoza <seecware@gmail.com>
2025-05-03 00:27:33 -0600

    Added html structure to layout. Ading some html layout, and removing an aproach of passing parent content to layout, now using render with wiew helper.

--- partials/layout.php
+++ partials/layout.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $title; ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <?php include __DIR__ . '/header.php'; ?>
     <div class="h-screen">
-        <?php echo $parent_content; ?>
+        <?php include __DIR__ . "/../views/{$name}.php"; ?>
     </div>
     <?php include __DIR__ . '/footer.php'; ?>
 </body>
