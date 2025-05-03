67f0356ca310ba4893ef4deb53a397c193a70069
Francisco Mendoza <seecware@gmail.com>
2025-05-03 00:26:27 -0600

    Fixing some anchors These pointed to a route that was not managed by our router internally..

--- partials/header.php
+++ partials/header.php
@@ -5,8 +5,9 @@
             <h1 class="text-xl ml-2 font-bold">phpRouter</h1>
         </div>
       <nav>
-        <a href="/index.php" class="text-blue-600 hover:underline mr-4">Inicio</a>
-        <a href="/pages/contacto.php" class="text-blue-600 hover:underline">Contacto</a>
+        <a href="/" class="text-blue-600 hover:underline mr-4">Inicio</a>
+        <a href="/contact" class="text-blue-600 hover:underline">Contacto</a>
       </nav>
     </div>
   </header>
+  
