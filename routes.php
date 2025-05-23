be3b7db852bb8d94a24c72da646d2a107773e6e9
Francisco .M <seecware@gmail.com>
2025-05-22 19:52:05 -0600

    Adding /pos route to manage view.

--- routes.php
+++ routes.php
@@ -5,6 +5,7 @@ return [
         '/contact' => ['StaticContentController', 'renderContent', 'contact', ["title" => "Contact"]],
         '/new-client' => ['StaticContentController', 'renderContent', 'new-client', ["title" => "Adding client..."]],
         '/new-added-client' => ['ClientsController', 'confirmation', 'new-added-client', ["title" => "Added!"]],
+        '/pos' => ['StaticContentController', 'renderContent', "pos", ['title' => 'POS']],
     ],
     'POST' => [
         '/new-client' => ['ClientsController', 'addNewClient'],
